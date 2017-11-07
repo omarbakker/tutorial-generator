21828d71a9d4c482658af9a2ddc0d72f705d90c3
Omar Mohammed <v0l0b@ece.ubc.ca>
2017-11-06 23:44:21 -0800

    Added script to fetch tutorial urls

---
+++ code/fetchSourceURLs.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+from requests import get
+
+print('Fetching tutorials')
+temp = 'https://www.raywenderlich.com/category/'
+categories = ['ios','macos','android','swift','apple-game-frameworks','unity']
+topURLs = [temp + category for category in categories]
+tutorialUrls = []
+
+def representsInt(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+def urlIsTutorialUrl(url):
+    url = url.replace('https://','').split('/')
+    return len(url) == 3 and representsInt(url[1])
+
+
+for url in topURLs:
+    result = get(url=url)
+    content = BeautifulSoup(result.content,
+                            "html.parser",
+                            parse_only=SoupStrainer('a'))
+    for atag in content:
+        if not atag.has_attr('href'): continue
+        url = atag['href']
+        if not urlIsTutorialUrl(url): continue
+        tutorialUrls.append(atag['href'])
+
+with open('data/tutorialURLs.txt', 'w') as urlFile:
+    urlFile.write('\n'.join(tutorialUrls))
+
+print('Found ', len(tutorialUrls), ' tutorials')
+print('Output in data/tutorialURLs.txt')
