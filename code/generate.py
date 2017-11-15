553df1029f7af3d8873329df0358345c3b541967
omarbakker <v0l0b@ece.ubc.ca>
2017-11-15 02:17:52 -0800

    Generate.py now works

--- code/generate.py
+++ code/generate.py
@@ -5,11 +5,10 @@ import utils
 INTERNAL_SIZE = 512
 N_LAYERS = 3
 
-tutor = "checkpoints/rnn_train_1510179844-51000000"
+tutor = "checkpoints/rnn_train_1510734663-78000000"
 mapdir = 'data/mapping.txt'
 rmapdir = 'data/reverseMapping.txt'
 
-# use topn=10 for all but the last one which works with topn=2 for Shakespeare and topn=3 for Python
 author = tutor
 
 ncnt = 0
@@ -17,12 +16,14 @@ with tf.Session() as sess, open(mapdir) as mapping, open(rmapdir) as reverseMapp
 
     mapping = mapping.readlines()
     reverseMapping = reverseMapping.readlines()
-    mapping = {l.split(':')[0]:l.split(':')[1] for l in mapping}
-    reverseMapping = {l.split(':')[0]:l.split(':')[1] for l in reverseMapping}
+    mapping = {l.split(':')[0]:l.split(':')[1].replace('\n','') for l in mapping}
+    reverseMapping = {l.split(':')[0]:l.split(':')[1].replace('\n','') for l in reverseMapping}
+    ALPHABET_SIZE = len(mapping) + 1
 
-    new_saver = tf.train.import_meta_graph('checkpoints/rnn_train_1510179844-51000000.meta')
+    new_saver = tf.train.import_meta_graph('checkpoints/rnn_train_1510734663-78000000.meta')
     new_saver.restore(sess, author)
-    x = reverseMapping.get(ord('<'))
+
+    x = reverseMapping.get(str(ord('<')))
     print('<', end="")
     x = np.array([[x]])
 
@@ -30,12 +31,12 @@ with tf.Session() as sess, open(mapdir) as mapping, open(rmapdir) as reverseMapp
     y = x
     h = np.zeros([1, INTERNAL_SIZE * N_LAYERS], dtype=np.float32)  # [ BATCHSIZE, INTERNALSIZE * NLAYERS]
     for i in range(1000000000):
-        # import pdb; pdb.set_trace()
+
         yo, h = sess.run(['Yo:0', 'H:0'], feed_dict={'X:0': y, 'pkeep:0': 1., 'Hin:0': h, 'batchsize:0': 1})
 
-        c = utils.sample_from_probabilities(yo, topn=10)
+        c = utils.sample_from_probabilities(yo, topn=10, ALPHABET_SIZE=ALPHABET_SIZE)
         y = np.array([[c]])  # shape [BATCHSIZE, SEQLEN] with BATCHSIZE=1 and SEQLEN=1
-        c = chr(mapping.get(c))
+        c = chr(int(mapping.get(str(c))))
         print(c, end="")
 
         if c == '\n':
